f3f9b8f5defea84786a769cd75da49e9006ce817
Victor Guerrero Bosch <vguerrerobosch@gmail.com>
2019-09-25 22:19:32 +0200

    ...

--- init.php
+++ init.php
@@ -4,7 +4,6 @@ require(dirname(__FILE__) . '/lib/Redsys.php');
 
 require(dirname(__FILE__) . '/lib/PaymentRequest.php');
 require(dirname(__FILE__) . '/lib/Webhook.php');
-require(dirname(__FILE__) . '/lib/WebhookSoap.php');
 
 require(dirname(__FILE__) . '/lib/Exception/SignatureVerificationException.php');
 
